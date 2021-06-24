a5581144306e616c5a23434d611dffad8774147d
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-06-24 20:44:59 +0300

    Правда или действие (часть 1) Правки

--- js/drawingThumbnails.js
+++ js/drawingThumbnails.js
@@ -8,9 +8,9 @@ export function drawingThumbnails (photos) {
     photosElement.querySelector('.picture__likes').textContent = photo.likes;
     photosElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
-    photosElement.querySelector('.picture').addEventListener('click', () => {
-      createFullSizePhoto(photo);
-    });
+    // photosElement.querySelector('.picture').addEventListener('click', () => {
+    //   createFullSizePhoto(photo);
+    // });
 
     pictures.appendChild(photosElement);
   });
@@ -19,48 +19,15 @@ export function drawingThumbnails (photos) {
 
 // домашка 7.2
 
-import { isEscEvent } from './util.js';
+// import { isEscEvent } from './util.js';
 
-export function createFullSizePhoto(picture) {
-  const bigPicture = document.querySelector('.big-picture');
-  const bigPictureImg = document.querySelector('.big-picture__img img');
-  const likesCount = document.querySelector('.likes-count');
-  const commentsCount = document.querySelector('.comments-count');
-  const bigPictureCancel = document.querySelector('.big-picture__cancel');
-  const body = document.querySelector('.body');
-
-  const closeModal = function () {
-    bigPictureCancel.addEventListener('click', () => {
-      bigPicture.classList.add('hidden');
-      isEscEvent();
-    });
-  };
-
-  closeModal();
-
-
-  bigPicture.classList.remove('hidden');
-  body.classList.add('modal.open');
-
-
-  bigPictureImg.src = picture.url;
-  likesCount.textContent = picture.textContent;
-  commentsCount.textContent = picture.textContent;
-}
-
-// export function createFullSizePhotos () {
-
+// export function createFullSizePhoto(picture) {
 //   const bigPicture = document.querySelector('.big-picture');
-//   const picturesP = document.querySelectorAll('.picture');
 //   const bigPictureImg = document.querySelector('.big-picture__img img');
 //   const likesCount = document.querySelector('.likes-count');
 //   const commentsCount = document.querySelector('.comments-count');
-//   // const socialComments = document.querySelector('.social__comments');
-//   const socialCaption = document.querySelector('.social__caption');
-//   const socialCommentCount = document.querySelector('.social__comment-count');
-//   const commentsLoader = document.querySelector('.comments-loader');
-//   const body = document.querySelector('.body');
 //   const bigPictureCancel = document.querySelector('.big-picture__cancel');
+//   const body = document.querySelector('.body');
 
 //   const closeModal = function () {
 //     bigPictureCancel.addEventListener('click', () => {
@@ -71,25 +38,13 @@ export function createFullSizePhoto(picture) {
 
 //   closeModal();
 
-//   picturesP.forEach((picture) => {
-//     picture.addEventListener('click', () => {
-//       bigPicture.classList.remove('hidden');
-//       socialCommentCount.classList.add('hidden');
-//       commentsLoader.classList.add('hidden');
-//       body.classList.add('modal.open');
 
-//       bigPictureImg.src = picture.url;
-//       likesCount.textContent = picture.textContent;
-//       commentsCount.textContent = picture.textContent;
-//       //     socialComments = `<li class="social__comment">
-//       //     <img
-//       //         class="social__picture"
-//       //         src="{{аватар}}"
-//       //         alt="{{имя комментатора}}"
-//       //         width="35" height="35">
-//       //     <p class="social__text">{{текст комментария}}</p>
-//       // </li>`;
-//       socialCaption.textContent = picture.description;
-//     });
-//   });
+//   bigPicture.classList.remove('hidden');
+//   body.classList.add('modal.open');
+
+
+//   bigPictureImg.src = picture.url;
+//   likesCount.textContent = picture.textContent;
+//   commentsCount.textContent = picture.textContent;
 // }
+
